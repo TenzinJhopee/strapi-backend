c8486fca853939dff12c765b83a0753dff5aa3a6
Tenzin Sean <tenzin.jeunesse@gmail.com>
2021-07-27 03:15:35 +0200

    added one

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6104678df38b5d39bc15719c9fcd6851'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bf7a16b6-be0b-4e51-83b3-7f532d319b17'
+};
